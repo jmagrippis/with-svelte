bcaeaac5bd2f8f4c260fb352872292a99c670ae2
Johnny Magrippis <j@magrippis.com>
2021-03-28 13:10:34 +0100

    🚑 pass node-fetch to graphql-request

--- src/lib/repositories/contentfulClient.ts
+++ src/lib/repositories/contentfulClient.ts
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch'
 import {GraphQLClient} from 'graphql-request/dist/index.js'
 
 if (
@@ -12,6 +13,7 @@ if (
 const endpoint = import.meta.env.VITE_CONTENTFUL_ENDPOINT as string
 const token = import.meta.env.VITE_CONTENTFUL_DELIVERY_TOKEN
 export const contentfulClient = new GraphQLClient(endpoint, {
+  fetch,
   headers: {
     Authorization: `Bearer ${token}`,
   },
