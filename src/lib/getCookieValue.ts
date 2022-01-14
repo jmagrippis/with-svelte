9df9312efdd452a7e5a816684591fa32350a3b03
Johnny Magrippis <j@magrippis.com>
2022-01-14 23:13:05 +0100

    🔥 nuke code in prep of video

--- src/lib/getCookieValue.ts
+++
@@ -1,2 +0,0 @@
-export const getCookieValue = (cookie: string, name: string): string | null =>
-	cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || null
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
 	import '../app.css'
 
-	import {theme} from '$lib/stores/theme'
 	import Header from '$lib/components/Header.svelte'
 	import Footer from '$lib/components/Footer.svelte'
 </script>
 
-<div id="theme-container" class={$theme}>
-	<div id="app-container">
-		<Header />
+<Header />
 
-		<main class="flex-grow flex flex-col items-center justify-center">
-			<slot />
-		</main>
+<main class="flex-grow flex flex-col items-center justify-center">
+	<slot />
+</main>
 
-		<Footer />
-	</div>
-</div>
+<Footer />
