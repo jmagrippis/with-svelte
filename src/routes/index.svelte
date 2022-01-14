3493c1ed229179e1462b91e0602b74e5cc20aff0
Johnny Magrippis <j@magrippis.com>
2022-01-14 22:07:13 +0100

    ♻️  tweaks upon review

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-  import Hero from '$lib/components/Hero/Hero.svelte'
+	import Hero from '$lib/components/Hero/Hero.svelte'
 </script>
 
+<svelte:head>
+	<title>With Svelte: Learn how to do fullstack with Svelte!</title>
+</svelte:head>
+
 <main>
-  <Hero />
+	<Hero />
 </main>
