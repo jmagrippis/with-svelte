28af5a7c01d75692012b59e039147389e32f14cd
Johnny Magrippis <j@magrippis.com>
2022-01-16 20:00:53 +0100

    ✨  add new lesson on an SVG & CSS Hamburger Menu

--- src/routes/lessons/index.json.ts
+++ src/routes/lessons/index.json.ts
@@ -3,12 +3,14 @@ import type {RequestHandler} from '@sveltejs/kit'
 // GET /lessons.json
 export const get: RequestHandler = async () => {
 	const relativePathsToContent = import.meta.globEager('./content/*.md')
-	const lessons = Object.entries(relativePathsToContent).map(
-		([path, {attributes}]) => ({
+	const lessons = Object.entries(relativePathsToContent)
+		.map(([path, {attributes}]) => ({
 			attributes,
 			href: `/lessons/${path.match(/\.\/content\/_(.+).md/)[1]}`,
-		})
-	)
+		}))
+		.sort((a, b) =>
+			new Date(a.attributes.date) > new Date(b.attributes.date) ? -1 : 1
+		)
 
 	return {
 		body: {lessons},
