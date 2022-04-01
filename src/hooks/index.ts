5bfc27ef87cb599697c9cb228823625e69db4a61
Johnny Magrippis <j@magrippis.com>
2022-04-01 23:13:44 +0200

    ✨  auto-refresh session if it's 6 days from expiry

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,21 +1,46 @@
-import {getIdTokenFromSessionCookie} from '$lib/firebase/admin'
-import {getCookieValue} from '$lib/getCookieValue'
 import type {GetSession, Handle} from '@sveltejs/kit'
+
+import {ONE_DAY_IN_SECONDS, ONE_WEEK_IN_SECONDS} from '$lib/constants'
+import {
+	createSessionCookieForUserId,
+	getIdTokenFromSessionCookie,
+} from '$lib/firebase/admin'
+import {getCookieValue} from '$lib/getCookieValue'
+
 import {isTheme} from '../types'
+import type {DecodedIdToken} from 'firebase-admin/auth'
 
 const getThemeFromCookie = (cookie: string) => {
 	const theme = getCookieValue(cookie, 'theme')
 	return isTheme(theme) ? theme : null
 }
 
+const SIX_DAYS_IN_SECONDS = ONE_DAY_IN_SECONDS * 6
+
+const shouldRefreshToken = (token: DecodedIdToken | null) =>
+	token && token.exp - Date.now() / 1000 < SIX_DAYS_IN_SECONDS
+
 export const handle: Handle = async ({event, resolve}) => {
 	const cookie = event.request.headers.get('cookie')
-	event.locals.theme = getThemeFromCookie(cookie)
-	event.locals.idToken = await getIdTokenFromSessionCookie(
+	const token = await getIdTokenFromSessionCookie(
 		getCookieValue(cookie, 'session')
 	)
 
-	return resolve(event)
+	event.locals.theme = getThemeFromCookie(cookie)
+	event.locals.idToken = token
+
+	const response = await resolve(event)
+
+	if (!response.headers.get('set-cookie') && shouldRefreshToken(token)) {
+		const freshSessionCookie = await createSessionCookieForUserId(
+			token.uid,
+			ONE_WEEK_IN_SECONDS
+		)
+
+		response.headers.set('set-cookie', freshSessionCookie)
+	}
+
+	return response
 }
 
 export const getSession: GetSession = ({locals}) => {
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const ONE_DAY_IN_SECONDS = 24 * 60 * 60
+export const ONE_WEEK_IN_SECONDS = 7 * ONE_DAY_IN_SECONDS
