f1287eece392e43879f13c19bcac36fa66e5ff47
Johnny Magrippis <j@magrippis.com>
2022-05-11 16:33:09 +0200

    ♻️  use the vitest options in svelte config

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,3 @@
 import {extractFromSvelteConfig} from 'vitest-svelte-kit'
 
-export default extractFromSvelteConfig({
-	kit: {
-		vite: {
-			test: {
-				mockReset: true,
-			},
-		},
-	},
-})
+export default extractFromSvelteConfig()
