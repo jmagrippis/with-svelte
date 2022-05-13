80f64af25f028d5cc245edd017d920bc024403e8
Johnny Magrippis <j@magrippis.com>
2022-05-13 15:44:58 +0200

    ✨ introduce a Fancy Currency Input Element

--- src/lib/components/FancyCurrency/FancyCurrency.svelte
+++ src/lib/components/FancyCurrency/FancyCurrency.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
+	import {formatCurrency} from './formatCurrency'
+
+	let value: null | string = null
+
+	$: formattedValue = value ? formatCurrency(parseFloat(value)) : null
 </script>
 
-<div>IMPLEMENT HERE</div>
+<label for="amount" class="relative mb-4">How much?</label>
+<span
+	class="relative mb-2 block text-4xl before:pointer-events-none before:absolute before:left-2 before:top-1 before:z-10 before:content-[attr(data-formatted-value)] focus:before:hidden"
+	data-formatted-value={formattedValue}
+	data-testid="formatted-value"
+>
+	<input
+		id="amount"
+		name="amount"
+		class="focus:text relative rounded py-1 px-2 text-transparent shadow focus:z-20 focus:text-copy-base"
+		placeholder="£5.00"
+		type="number"
+		pattern="\d*\.?\d*"
+		bind:value
+	/>
+</span>
+
+<style>
+	input::outer-spin-button,
+	input::-webkit-inner-spin-button {
+		appearance: none;
+	}
+</style>
--- src/lib/components/FancyCurrency/FancyCurrency.test.ts
+++ src/lib/components/FancyCurrency/FancyCurrency.test.ts
@@ -1,7 +1,49 @@
 import {render} from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 
 import FancyCurrency from './FancyCurrency.svelte'
 
-it('allows the user to type into it', () => {
-	render(FancyCurrency)
+it('allows the user to type into it', async () => {
+	const user = userEvent.setup()
+	const {getByLabelText, getByDisplayValue, getByPlaceholderText, getByTestId} =
+		render(FancyCurrency)
+
+	expect(getByPlaceholderText('£5.00')).toBeInTheDocument()
+	const $input = getByLabelText('How much?')
+	expect($input).toBeInTheDocument()
+
+	await user.type($input, '1337')
+
+	const $updatedInput = getByDisplayValue('1337')
+	expect($updatedInput).toBeInTheDocument()
+
+	const $formattedValue = getByTestId('formatted-value')
+
+	expect($formattedValue).toHaveAttribute('data-formatted-value', '£1,337.00')
+
+	await user.clear($updatedInput)
+
+	await user.type($updatedInput, '13.37')
+
+	const $updatedInputB = getByDisplayValue('13.37')
+	expect($updatedInputB).toBeInTheDocument()
+
+	const $updatedFormattedValue = getByTestId('formatted-value')
+
+	expect($updatedFormattedValue).toHaveAttribute(
+		'data-formatted-value',
+		'£13.37'
+	)
+})
+
+it('does not allow the user to type in characters', async () => {
+	const user = userEvent.setup()
+	const {getByLabelText, getByDisplayValue} = render(FancyCurrency)
+
+	const $input = getByLabelText('How much?')
+	expect($input).toBeInTheDocument()
+
+	await user.type($input, '42w1`')
+
+	const $updatedInput = getByDisplayValue('421')
 })
