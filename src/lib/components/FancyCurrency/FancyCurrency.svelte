8de5be46b76b9cf082ee68f19e2ea80f3b6d8f5c
Johnny Magrippis <j@magrippis.com>
2022-05-18 18:19:22 +0200

    ♻️  use a "before" pseudo-element in the label instead of in a separate span

--- src/lib/components/FancyCurrency/FancyCurrency.svelte
+++ src/lib/components/FancyCurrency/FancyCurrency.svelte
@@ -12,16 +12,15 @@
 	$: formattedValue = value ? formatCurrency(parseFloat(value)) : null
 </script>
 
-<label for="amount" class="relative mb-4 text-2xl text-copy-muted"
-	>How much you wanna tip?</label
->
-<span
-	class="relative mb-2 block text-4xl before:pointer-events-none before:absolute before:left-2 before:top-1 before:z-10 before:content-[attr(data-formatted-value)] focus:before:hidden"
+<label
+	class="relative mb-4 block text-4xl before:pointer-events-none before:absolute before:left-2 before:top-11 before:z-10 before:content-[attr(data-formatted-value)] focus:before:hidden"
 	data-formatted-value={formattedValue}
 	data-testid="formatted-value"
 >
+	<span class="block h-10 select-none text-2xl text-copy-muted"
+		>How much you wanna tip?</span
+	>
 	<input
-		id="amount"
 		name="amount"
 		class="focus:text relative w-full rounded py-1 px-2 text-transparent shadow focus:z-20 focus:text-copy-base"
 		placeholder="£5.00"
@@ -29,7 +28,7 @@
 		pattern="\d*\.?\d*"
 		bind:value
 	/>
-</span>
+</label>
 
 <div class="mb-4 text-xl">
 	Suggested amounts
